7a73166468cc62089fe93efe3b91effed2ef2d64
espamv <florian.jess2006@gmail.com>
2025-12-06 15:35:37 +0100

    refactor ReplaySystem and build.gradle to remove unused S3 dependency and clean up code

--- src/main/java/me/jumper251/replay/ReplaySystem.java
+++ src/main/java/me/jumper251/replay/ReplaySystem.java
@@ -1,14 +1,7 @@
 package me.jumper251.replay;
 
-
-import java.util.HashMap;
-
-
 import com.alessiodp.libby.BukkitLibraryManager;
-import com.alessiodp.libby.Library;
-import me.jumper251.replay.filesystem.saving.S3ReplaySaver;
-import org.bukkit.plugin.java.JavaPlugin;
-
+import java.util.HashMap;
 import me.jumper251.replay.database.DatabaseRegistry;
 import me.jumper251.replay.filesystem.ConfigManager;
 import me.jumper251.replay.filesystem.saving.DatabaseReplaySaver;
@@ -18,84 +11,60 @@ import me.jumper251.replay.replaysystem.Replay;
 import me.jumper251.replay.replaysystem.utils.ReplayCleanup;
 import me.jumper251.replay.utils.Metrics;
 import me.jumper251.replay.utils.ReplayManager;
-
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class ReplaySystem extends JavaPlugin {
 
-	
-	public static ReplaySystem instance;
-	
-	public static Metrics metrics;
-	
-	public final static String PREFIX = "§8[§3Replay§8] §r§7";
+  public static ReplaySystem instance;
 
-	
-	@Override
-	public void onDisable() {
-		for (Replay replay : new HashMap<>(ReplayManager.activeReplays).values()) {
-		    if (replay.isRecording() && replay.getRecorder().getData().getActions().size() > 0) {
-				replay.getRecorder().stop(ConfigManager.SAVE_STOP);
-				
-			}
-		}
+  public static Metrics metrics;
+  public final static String PREFIX = "§8[§3Replay§8] §r§7";
 
-	}
-	
-	@Override
-	public void onEnable() {
-		instance = this;
-		
-		Long start = System.currentTimeMillis();
 
-		BukkitLibraryManager libraryManager = new BukkitLibraryManager(this);
-		libraryManager.addMavenCentral();
+  @Override
+  public void onDisable() {
+    for (Replay replay : new HashMap<>(ReplayManager.activeReplays).values()) {
+      if (replay.isRecording() && !replay.getRecorder().getData().getActions().isEmpty()) {
+        replay.getRecorder().stop(ConfigManager.SAVE_STOP);
+      }
+    }
+  }
 
-		getLogger().info("Loading Replay v" + getDescription().getVersion() + " by " + getDescription().getAuthors().get(0));
-		
-		ConfigManager.loadConfigs();
-		ReplayManager.register();
+  @Override
+  public void onEnable() {
+    instance = this;
 
-		if (ConfigManager.USE_DATABASE) {
-			ReplaySaver.register(new DatabaseReplaySaver());
-			DatabaseRegistry.getDatabase().getService().getReplays().stream()
-					.forEach(info -> DatabaseReplaySaver.replayCache.put(info.getID(), info));
-		} else if (ConfigManager.USE_S3) {
-			Library minioLibrary = Library.builder()
-					.groupId("io{}minio")
-					.artifactId("minio")
-					.version("8.5.12")
-					.resolveTransitiveDependencies(true)
-					.build();
-			libraryManager.loadLibrary(minioLibrary);
-			S3ReplaySaver s3ReplaySaver = new S3ReplaySaver(
-					ConfigManager.s3Cfg.getString("endpoint_url"),
-					ConfigManager.s3Cfg.getString("access_key"),
-					ConfigManager.s3Cfg.getString("secret_key"),
-					ConfigManager.s3Cfg.getString("bucket_name")
-			);
+    var start = System.currentTimeMillis();
 
-			s3ReplaySaver.connect().thenAccept(isConnected -> {
-				if (!isConnected)
-					getLogger().warning("Could not connect to S3 storage backend.");
-			});
+    BukkitLibraryManager libraryManager = new BukkitLibraryManager(this);
+    libraryManager.addMavenCentral();
 
-			ReplaySaver.register(s3ReplaySaver);
-		} else {
-			ReplaySaver.register(new DefaultReplaySaver());
-		}
+    getLogger().info(
+        "Loading Replay v" + getDescription().getVersion() + " by " + getDescription().getAuthors()
+            .get(0));
 
-		metrics = new Metrics(this, 2188);
-		
-		if (ConfigManager.CLEANUP_REPLAYS > 0) {
-			ReplayCleanup.cleanupReplays();
-		}
-		
-		getLogger().info("Finished (" + (System.currentTimeMillis() - start) + "ms)");
+    ConfigManager.loadConfigs();
+    ReplayManager.register();
 
-	}
-	
-	
-	public static ReplaySystem getInstance() {
-		return instance;
-	}
+    if (ConfigManager.USE_DATABASE) {
+      ReplaySaver.register(new DatabaseReplaySaver());
+      DatabaseRegistry.getDatabase().getService().getReplays()
+          .forEach(info -> DatabaseReplaySaver.replayCache.put(info.getID(), info));
+    } else {
+      ReplaySaver.register(new DefaultReplaySaver());
+    }
+
+    metrics = new Metrics(this, 2188);
+
+    if (ConfigManager.CLEANUP_REPLAYS > 0) {
+      ReplayCleanup.cleanupReplays();
+    }
+
+    getLogger().info("Finished (" + (System.currentTimeMillis() - start) + "ms)");
+
+  }
+
+  public static ReplaySystem getInstance() {
+    return instance;
+  }
 }
